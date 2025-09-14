5d5fb516bdf9e2cd1281750285eb35483cf13545
Viliane <vilianeso@gmail.com>
2025-09-13 22:32:05 -0300

    criacao microserviço

---
+++ src/services/AcademyIO.Courses.API/Models/ICourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using AcademyIO.Core.Data;
+
+namespace AcademyIO.Courses.API.Models
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+        Task<IEnumerable<Course>> GetAll();
+
+        Task<Course> GetById(Guid courseId);
+
+        void Add(Course course);
+
+        bool CourseExists(Guid courseI);
+    }
+}
