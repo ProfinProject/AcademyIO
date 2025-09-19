d82fb748679a7f7c0af13af93e217820f295ee9e
Fabiano Marcolin Maciel <fmaciel@glasslewis.com>
2025-09-19 21:16:01 +0100

    concertando a estrutura de pastas

--- src/api-gateways/AcademyIO.Bff/Controllers/StudentsController.cs
+++ src/api-gateways/AcademyIO.Bff/Controllers/StudentsController.cs
@@ -1,13 +1,16 @@
 ﻿using AcademyIO.Bff.Services;
+using AcademyIO.WebAPI.Core.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AcademyIO.Bff.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StudentsController : ControllerBase
+    public class StudentsController : MainController
     {
         private readonly IStudentService _studentService;
         private readonly ICourseService _courseService;
+
+
     }
 }
