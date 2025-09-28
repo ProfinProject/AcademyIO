30fe311e91d06d00dd62bbdf0633308336804ca1
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2025-09-28 16:14:50 -0400

    feature: estrutura de apresentação inicial do site

--- src/Front-End/src/app/app.routes.ts
+++ src/Front-End/src/app/app.routes.ts
@@ -3,12 +3,20 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { NotFound } from './features/shared/not-found/not-found';
 import { Sobre } from './public/sobre/sobre';
+import { Cursos } from './features/cursos/cursos';
+import { Contato } from './public/contato/contato';
+import { Login } from './public/login/login';
+import { Registrar } from './public/registrar/registrar';
 
 
 
 export const routes: Routes = [
     {path:'home', component: Home}
      ,{path:'sobre', component: Sobre}
+    ,{path:'lista-cursos', component: Cursos}
+    ,{path:'Contato', component: Contato}
+    ,{path:'login', component: Login}
+    ,{path:'registrar', component: Registrar}
     ,{path:'not-found', component: NotFound}
     ,{ path: '', redirectTo: '/home', pathMatch: 'full' }
     ,{ path: '**', redirectTo: '/not-found' } 
